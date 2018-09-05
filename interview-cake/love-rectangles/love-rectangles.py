9838c2d041d9bef9614f73a7e808bc0af885d3a9
Matt Davis <jiffyclub@gmail.com>
2018-09-04 22:33:54 -0700

    Add love rectangles bonus examples

--- interview-cake/love-rectangles/love-rectangles.py
+++ interview-cake/love-rectangles/love-rectangles.py
@@ -24,27 +24,39 @@ Your output rectangle should use this format as well.
 
 """
 
-from collections import namedtuple
+from functools import reduce
+from itertools import combinations
+from pprint import pprint
 
-Rectangle = namedtuple('Rectangle', ['bottom', 'top', 'left', 'right'])
+from dataclasses import dataclass
+
+
+@dataclass
+class Rectangle:
+    bottom: int
+    top: int
+    left: int
+    right: int
 
 
 def rect_intersection(rect1: Rectangle, rect2: Rectangle) -> Rectangle:
     """
     Returns the intersection of two rectangles as a rectangle description.
 
-    Returns None if they have no intersection.
+    Returns Rectangle(-2, -1, -2, -1) if they have no intersection.
+    (For compatibility with reduce.)
+    (Makes this incompatible with rectangles in negative space.)
 
     """
     if (rect1.bottom >= rect2.top
             or rect2.bottom >= rect1.top
             or rect1.left >= rect2.right
             or rect2.left >= rect1.right):
-        return None
+        return Rectangle(-2, -1, -2, -1)
 
     left = max(rect1.left, rect2.left)
     bottom = max(rect1.bottom, rect2.bottom)
-    top = min(rect1.top, rect1.top)
+    top = min(rect1.top, rect2.top)
     right = min(rect1.right, rect2.right)
 
     assert bottom < top, f'{bottom} > {top}'
@@ -63,16 +75,16 @@ def test(rect1, rect2, expected):
 
 
 # None
-test(Rectangle(1, 2, 1, 2), Rectangle(0, 1, 0, 1), None)
+test(Rectangle(1, 2, 1, 2), Rectangle(0, 1, 0, 1), Rectangle(-2, -1, -2, -1))
 
 # None
-test(Rectangle(1, 2, 1, 2), Rectangle(2, 3, 1, 2), None)
+test(Rectangle(1, 2, 1, 2), Rectangle(2, 3, 1, 2), Rectangle(-2, -1, -2, -1))
 
 # None
-test(Rectangle(1, 2, 1, 2), Rectangle(100, 200, 100, 200), None)
+test(Rectangle(1, 2, 1, 2), Rectangle(100, 200, 100, 200), Rectangle(-2, -1, -2, -1))
 
 # None
-test(Rectangle(100, 200, 100, 200), Rectangle(1, 2, 1, 2), None)
+test(Rectangle(100, 200, 100, 200), Rectangle(1, 2, 1, 2), Rectangle(-2, -1, -2, -1))
 
 # Rectangle(1, 2, 1, 2)
 test(Rectangle(1, 2, 1, 2), Rectangle(0, 2, 0, 2), Rectangle(1, 2, 1, 2))
@@ -82,3 +94,28 @@ test(Rectangle(1, 2, 1, 2), Rectangle(1, 2, 1, 2), Rectangle(1, 2, 1, 2))
 
 # Rectangle(2, 4, 5, 7)
 test(Rectangle(1, 4, 1, 7), Rectangle(2, 6, 5, 8), Rectangle(2, 4, 5, 7))
+
+print()
+
+# Bonus Part 1
+rectangles = [
+    Rectangle(0, 2, 0, 2),  # (1, 2, 0, 2), (1, 2, 1, 2), (0, 2, 1, 2)
+    Rectangle(1, 3, 0, 2),  # (1, 3, 1, 2), (1, 2, 1, 2)
+    Rectangle(1, 3, 1, 3),  # (1, 2, 1, 3)
+    Rectangle(0, 2, 1, 3)
+]
+pprint(
+    [
+        rect_intersection(rect1, rect2)
+        for rect1, rect2 in combinations(rectangles, 2)
+    ]
+)
+
+print()
+
+# Bonus Part 2
+# Rectangle(1, 2, 1, 2)
+print(reduce(rect_intersection, rectangles))
+
+# Rectangle(-2, -1, -2, -1)
+print(reduce(rect_intersection, [Rectangle(0, 1, 0, 1), Rectangle(5, 6, 5, 6)]))
