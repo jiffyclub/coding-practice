79ee4978a8a789ec0b18388b124344515b2973a0
Matt Davis <jiffyclub@gmail.com>
2018-10-02 21:26:48 -0700

    Add find-duplicates description for later

---
+++ interview-cake/find-duplicates/find-duplicates.py
@@ -0,0 +1,17 @@
+"""
+List of integers:
+
+- in the range 1..n
+- list is n + 1 integers long
+
+Find an integer that's in the list more than once.
+
+Optimize for space.
+
+
+Idea 1: Use a counter. Uses 2 * N.
+
+"""
+
+
+
